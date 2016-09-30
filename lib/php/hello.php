d502cca735f39843d2324b2fb6fc4e5b165d338c
Todd Test <todd+testtest@gmail.com>
2016-09-30 17:47:21 -0400

    Add some terrible php code

--- lib/php/hello.php
+++ lib/php/hello.php
@@ -1,3 +1,35 @@
+<?php
+
 $some_string = "hello"
 
 echo $some_string
+
+function swap($a,$b){
+  if(true) {
+    if (true) {
+      if(true) {
+      if(true) {
+      if(true) {
+      if(true) {
+      if(true) {
+      if(true) {
+        echo "hello"
+}
+}}}}}}}
+}
+
+function other_swap($a,$b){
+  if(true) {
+    if (true) {
+      if(true) {
+      if(true) {
+      if(true) {
+      if(true) {
+      if(true) {
+      if(true) {
+        echo "hello"
+}
+}}}}}}}
+}
+
+?>
