cc3c489ca4eb794df82606f065a0dc491b0505b6
Todd Test <todd+testtest@gmail.com>
2016-09-30 13:52:30 -0400

    Add a terrible class

---
+++ lib/terrible_apples.rb
@@ -0,0 +1,13 @@
+class TerribleApples
+  def gross
+    if true
+      unless false
+        if true && true
+          unless not false && false
+            puts "what!"
+          end
+        end
+      end
+    end
+  end
+end
