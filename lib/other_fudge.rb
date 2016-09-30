9b20adea0ef067b8d4d31536a7ddb5fe85e89bf1
Todd Test <todd+testtest@gmail.com>
2016-09-30 14:58:08 -0400

    Add more problems

---
+++ lib/other_fudge.rb
@@ -0,0 +1,33 @@
+class OtherFudge
+  def say_fudge
+    puts "fudge"
+  end
+
+  def this_method_has_too_many_lines
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+    puts "fudge"
+  end
+end
