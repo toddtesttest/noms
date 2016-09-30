1077aa5844463b0c1c9e2dffe2095f0f4e71e30b
Todd Test <todd+testtest@gmail.com>
2016-09-30 13:56:17 -0400

    Make things a lot worse

--- lib/terrible_apples.rb
+++ lib/terrible_apples.rb
@@ -10,4 +10,24 @@ class TerribleApples
       end
     end
   end
+
+  def ick
+    if true
+      unless false
+        if true && true
+          unless not false && false
+            if true
+              unless false
+                if true && true
+                  unless not false && false
+                    puts "what!"
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
